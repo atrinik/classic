201227bed16f78267ab00a1272ce99c235a1eefd
Zoey Rose <zoey@zoeysr.com>
2026-08-13 07:53:31 +0000

    test(client): cover all server sound dispatches

--- client/src/tests/sound_command.c
+++ client/src/tests/sound_command.c
@@ -28,6 +28,7 @@ static int played_volume;
 static int played_loop;
 static int8_t played_x;
 static int8_t played_y;
+static uint8_t played_type;
 
 void sound_command_play_effect(const char *filename, int volume, int loop, int8_t x, int8_t y) {
     snprintf(VS(played_filename), "%s", filename);
@@ -35,20 +36,21 @@ void sound_command_play_effect(const char *filename, int volume, int loop, int8_
     played_loop = loop;
     played_x = x;
     played_y = y;
+    played_type = CMD_SOUND_EFFECT;
 }
 
 void sound_command_play_background(const char *filename, int volume, int loop) {
-    (void)filename;
-    (void)volume;
-    (void)loop;
-    abort();
+    snprintf(VS(played_filename), "%s", filename);
+    played_volume = volume;
+    played_loop = loop;
+    played_type = CMD_SOUND_BACKGROUND;
 }
 
 void sound_command_play_absolute(const char *filename, int volume, int loop) {
-    (void)filename;
-    (void)volume;
-    (void)loop;
-    abort();
+    snprintf(VS(played_filename), "%s", filename);
+    played_volume = volume;
+    played_loop = loop;
+    played_type = CMD_SOUND_ABSOLUTE;
 }
 
 static void test_received_effect_filename_is_opaque(void) {
@@ -79,8 +81,10 @@ static void test_received_effect_filename_is_opaque(void) {
             packet[4 + filename_size] = 4;
 
             played_filename[0] = '\0';
+            played_type = 0xff;
             socket_command_sound(packet, packet_size, 0);
             TEST_CHECK(played_filename[0] != '\0');
+            TEST_CHECK(played_type == CMD_SOUND_EFFECT);
             TEST_CHECK(strcmp(played_filename, filenames[i]) == 0);
             TEST_CHECK(played_volume == -17);
             TEST_CHECK(played_loop == -1);
@@ -90,7 +94,31 @@ static void test_received_effect_filename_is_opaque(void) {
     }
 }
 
+static void test_received_non_effect_dispatch(void) {
+    static const uint8_t types[] = {CMD_SOUND_BACKGROUND, CMD_SOUND_ABSOLUTE};
+
+    for (size_t i = 0; i < arraysize(types); i++) {
+        static const char filename[] = "server-selected.ogg";
+        uint8_t packet[64];
+        size_t filename_size = sizeof(filename);
+        size_t packet_size = 1 + filename_size + 2;
+        packet[0] = types[i];
+        memcpy(&packet[1], filename, filename_size);
+        packet[1 + filename_size] = 3;
+        packet[2 + filename_size] = (uint8_t)-9;
+
+        played_filename[0] = '\0';
+        played_type = 0xff;
+        socket_command_sound(packet, packet_size, 0);
+        TEST_CHECK(played_type == types[i]);
+        TEST_CHECK(strcmp(played_filename, filename) == 0);
+        TEST_CHECK(played_volume == -9);
+        TEST_CHECK(played_loop == 3);
+    }
+}
+
 int main(void) {
     test_received_effect_filename_is_opaque();
+    test_received_non_effect_dispatch();
     return 0;
 }
