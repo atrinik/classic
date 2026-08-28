842ee2ab7fa93f98a8d6e914e54d2cfe12c74c94
Zoey Rose <zoey@zoeysr.com>
2026-08-28 08:58:50 +0000

    fix(client): preserve GPU recovery diagnostics

--- client/src/client/main.c
+++ client/src/client/main.c
@@ -649,15 +649,13 @@ static bool gpu_renderer_recover_frame(unsigned int *attempts, const char *conte
     if (*attempts < 1U) {
         (*attempts)++;
         if (gpu_renderer_recover(ScreenWindow)) {
-            if (!resize_window_recovery_apply()) {
-                SDL_SetError("unable to query the recovered GPU output size");
-                return false;
+            if (resize_window_recovery_apply()) {
+                map_redraw_request(MAP_REDRAW_REASON_EXTERNAL);
+                minimap_redraw_flag = 1;
+                widget_redraw_everything();
+                popup_redraw_all();
+                return true;
             }
-            map_redraw_request(MAP_REDRAW_REASON_EXTERNAL);
-            minimap_redraw_flag = 1;
-            widget_redraw_everything();
-            popup_redraw_all();
-            return true;
         }
     }
 
